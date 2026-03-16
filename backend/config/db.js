2b013dce6d381a8d6b6d7d3f41e550ecce5111d6
Mohammad Faisal <mohdfaisal199754.mf@gmail.com>
2026-03-15 20:02:02 -0700

    update new

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
 
-  host: "MYSQL8002.site4now.net",
+  host: "mysql5036.site4now.net",
   user: "ac6b1d_account",
   password: "Faisal@1997",
   database: "db_ac6b1d_account"
