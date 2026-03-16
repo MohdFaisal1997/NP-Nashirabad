11ab9e4ac5590492402bd4df564b4e0844f451db
Mohammad Faisal <mohdfaisal199754.mf@gmail.com>
2026-03-15 21:04:55 -0700

    add env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,52 @@
+// const mysql = require("mysql2");
+
+// const db = mysql.createConnection({
+
+  
+
+//   host: "localhost",
+//   user: "root",
+//   password: "Faisal@1997",
+//   database: "np_accounting"
+
+// });
+
+// db.connect((err)=>{
+
+//   if(err){
+//     console.log("Database connection error:", err);
+//   }
+//   else{
+//     console.log("MySQL Connected");
+//   }
+
+// });
+
+// module.exports = db;
+
+
+
+
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-
-  host: "MYSQL5036.site4now.net",
-  user: "ac6b1d_account",
-  password: "Faisal@1997",
-  database: "db_ac6b1d_account"
-
-  // host: "localhost",
-  // user: "root",
-  // password: "Faisal@1997",
-  // database: "np_accounting"
-
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect((err)=>{
-
-  if(err){
+db.connect((err) => {
+  if (err) {
     console.log("Database connection error:", err);
-  }
-  else{
+  } else {
     console.log("MySQL Connected");
   }
-
 });
 
 module.exports = db;
